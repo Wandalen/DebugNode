6c556ffda7495cc13165e7a326357a4fe99b5832
wandalen <wandalen.me@gmail.com>
2020-10-05 09:25:56 +0300

    .

--- sample/Sample.s
+++ sample/Sample.s
@@ -2,7 +2,7 @@
 let _ = require( 'wprocess' );
 
 debugger
-_.process.startNode({ execPath : _.path.join( __dirname, 'Sample2.s' ), mode : 'spawn', stdio : 'inherit' })
+_.process.startNjs({ execPath : _.path.join( __dirname, 'Sample2.s' ), mode : 'spawn', stdio : 'inherit' })
 .thenKeep( () =>
 {
   debugger
