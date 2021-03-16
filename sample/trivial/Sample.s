86930c18a1d6e3c855a50c787cb894b65bd62490
Jack Shell <jackshel45@gmail.com>
2021-03-16 13:36:47 +0200

    Fixed to pass tests

--- sample/trivial/Sample.s
+++ sample/trivial/Sample.s
@@ -1,10 +1,8 @@
 
 let _ = require( 'wprocess' );
 
-debugger
 _.process.startNjs({ execPath : _.path.nativize( _.path.join( __dirname, 'Sample2.s' ) ), mode : 'spawn', stdio : 'inherit' })
 .thenKeep( () =>
 {
-  debugger
   return null;
 })
