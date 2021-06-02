ac8531ad3fd5dc5d33635d83483412103de7c0c4
volodymyrmigdal <volodymyr.migdal@gmail.com>
2021-06-02 09:47:19 +0300

    update samples

--- sample/trivial/Sample2.s
+++ sample/trivial/Sample2.s
@@ -1,6 +1,6 @@
 
-let _ = require( 'wprocess' );
-
+let _ = require( 'wTools' );
+_.include( 'wProcess' );
 
 _.process.startNjs({ execPath : _.path.nativize( _.path.join( __dirname, 'Sample3.s' ) ), mode : 'spawn', stdio : 'inherit' })
 .thenKeep( () =>
